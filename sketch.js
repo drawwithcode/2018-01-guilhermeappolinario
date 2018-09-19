b720d91214e23e8b1f5c339e7bdb86dd188f4db8
Guilherme Appolinario <guilhermeappolinario@gmail.com>
2018-09-19 17:27:39 +0200

    go

--- sketch.js
+++ sketch.js
@@ -1,23 +1,58 @@
+  function preload(){
+  // put preload code here
+}
+
 function setup() {
-	// define artboard size
-	createCanvas(500,500);
-	// Set here the values that are not changing all over the sketch
-	strokeWeight(2);
-	stroke(170);
+  // put setup ccode here
+  createCanvas(1280,720);
 }
 
 function draw() {
-	background(204);
+  // put drawing
+  background(0,0,0)
+  // Neck
 
-	//Neck
-	line(250, 285, 250, 345);
+  stroke(255,255,255)
+  line(580+pmouseX/16,pmouseY/16+340,pmouseX,pmouseY+20)
 
-	//Wheel
-	ellipse(250, 465, 70, 70);
+  stroke(255,255,255)
+  line(600+pmouseX/16,pmouseY/16+340,pmouseX,pmouseY+20)
 
-	//Body
-	rect(200, 325, 100, 140);
+  stroke(255,255,255)
+  line(620+pmouseX/16,pmouseY/16+340,pmouseX,pmouseY+20)
+   // Antennas
 
-	//Head
-	ellipse(250, 240, 90, 90);
+   // Body
+   rectMode(CENTER);
+      fill(192);
+      arc(600+pmouseX/16, pmouseY/16+100+400, 280, 280, PI, TWO_PI);
+
+      fill(255,255,255);
+      rect(600+pmouseX/16,pmouseY/16+100+340,120,200,20);
+
+
+   // Head
+      // antennas
+      stroke(255,255,255)
+      line(pmouseX-100,pmouseY-100,pmouseX-10,pmouseY-10)
+
+      stroke(255,255,255)
+      line(pmouseX+100,pmouseY-100,pmouseX-10,pmouseY-10)
+      // white circle
+      noStroke();
+      fill(255,255,255)
+      ellipse(pmouseX-0,pmouseY+0,100)
+      // eye
+      fill(128,128,128)
+      ellipse(pmouseX-25,pmouseY,25)
+
+      fill(128,128,128)
+      ellipse(pmouseX+25,pmouseY,25)
+      // pupils
+      fill(40,40,40)
+      ellipse(pmouseX-25,pmouseY,5)
+
+      fill(40,40,40)
+      ellipse(pmouseX+25,pmouseY,5)
+
 }
